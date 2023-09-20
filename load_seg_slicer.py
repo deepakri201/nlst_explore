c8982eee2d2e78dd0b6c379fc443b69ef8343ad3
Deepa Krishnaswamy <deepa@alberta.ca>
2023-09-19 20:48:07 -0400

    can load seg and ct into DICOM database and into scene automatically

--- load_seg_slicer.py
+++ load_seg_slicer.py
@@ -1,8 +1,8 @@
 # load_seg_slicer.py 
 # 
-# This script takes as input a manifest txt file with a list of SeriesInstanceUIDs, 
-# which each correspond to the SeriesInstanceUID of the DICOM segmentation object. 
-# 1. Each series is imported from the DICOM datastore. 
+# 1. This script takes as input the DICOM Segmentation object studyInstanceUID, 
+#    seriesInstanceUID and SOPInstanceUID. It downloads the object from the DICOM 
+#    datastore.  
 # 2. The corresponding referenceSeriesInstanceUID is found for each series, and 
 #    obtained from the IDC bucket. 
 # 3. The DICOM files are read into the Slicer DICOM database. 
@@ -20,8 +20,10 @@
 # 
 # cd to C:\Users\deepa\AppData\Local\slicer.org\Slicer 5.5.0-2023-08-19
 # Slicer.exe --python-script "C://Users//deepa//git//nlst_explore//load_seg_slicer.py" # --no-splash  --no-main-window
-
 # 
+# To do: 
+# - change names of files that are written, so not to the same folder. Maybe name by SeriesInstanceUID instead? 
+# - retrieve the series instead of instance for the DICOM SEG object. Then don't need StudyInstanceUID or SOPInstanceUID. 
 #
 # Deepa Krishnaswamy 
 # Brigham and Women's Hospital
@@ -60,10 +62,6 @@ from google.cloud import storage
 #-- Inputs --# 
 ##############
 
-# Manifest - list of SeriesInstanceUIDs of the DICOM Segmentation objects 
-manifest_file = "C://Users//deepa//git//nlst_explore//manifest//manifest_list.txt"
-gcs_file = "C://Users//deepa//git//nlst_explore//manifest//gcs_list.txt" 
-
 # GCP settings 
 project_id = "idc-external-025"
 dataset_id = "total_segmentator_nlst_total_070823"
@@ -74,7 +72,7 @@ location_id = "us-central1"
 table_id = "bigquery-public-datasets.idc_current.dicom_all"
 
 ##############################
-#-- Read the manifest file --# 
+#-- Set the UIDs --# 
 ##############################
 
 studyInstanceUID = "1.2.840.113654.2.55.222358418596811829743750475747304078456"
@@ -99,9 +97,9 @@ headers["Authorization"] = f"Bearer {GoogleCloudPlatform().token()}"
 DICOMwebClient = DICOMwebClient(url=url, session=session, headers=headers)
 
 # first set the downLoad directory 
-downloadDirectory = os.path.join(slicer.dicomDatabase.databaseDirectory, 'tmp')
-if not os.path.isdir(downloadDirectory):
-  os.makedirs(downloadDirectory)
+downloadDirectory_seg = os.path.join(slicer.dicomDatabase.databaseDirectory, 'tmp_seg')
+if not os.path.isdir(downloadDirectory_seg):
+  os.makedirs(downloadDirectory_seg)
 
 # Retrieve the instance using the DICOM web client  
 retrievedInstance = DICOMwebClient.retrieve_instance(study_instance_uid=studyInstanceUID,
@@ -109,153 +107,80 @@ retrievedInstance = DICOMwebClient.retrieve_instance(study_instance_uid=studyIns
                                                      sop_instance_uid=sopInstanceUID)
 
 # Write the file to a tmp folder 
-pydicom.filewriter.write_file(os.path.join(downloadDirectory, "seg.dcm"), retrievedInstance)
+pydicom.filewriter.write_file(os.path.join(downloadDirectory_seg, "seg.dcm"), retrievedInstance)
 
 # Import file into the dicom database 
 db = slicer.dicomDatabase
-DICOMUtils.importDicom(downloadDirectory,db)
+DICOMUtils.importDicom(downloadDirectory_seg,db)
 
 #####################################################
 #-- Get the corresponding referenced series files --#
 #####################################################
 
-import subprocess
-output = subprocess.run(["gcloud", "auth", "application-default", "login"])
-print(output)
-# cmdCommand = "gcloud auth application-default login" 
-# process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
-# output, error = process.communicate()
-# print(output)
-# print(error)
+# Get the referenced SeriesInstanceUID
+dcm = pydicom.dcmread(os.path.join(downloadDirectory_seg, "seg.dcm"))
+referenced_series_instance_uid =  dcm.ReferencedSeriesSequence[0]['SeriesInstanceUID'].value
+print('referenced_series_instance_uid: ' + str(referenced_series_instance_uid))
 
-# # read the file with pydicom 
-#
-# # get the referenced SeriesInstanceUID 
-#
-# # use gsutil to download from the bucket 
-# # can use s5cmd later 
-#
-# # Select all files from GCS for a given DICOM series
-#
-# client = bigquery.Client(project=project_id)
-#
-# query = f"""
-# SELECT 
-#   DISTINCT(CONCAT("cp s3://", SPLIT(gcs_url,"/")[SAFE_OFFSET(2)], "/", crdc_series_uuid, "/* .")) 
-# FROM 
-#     `bigquery-public-data.idc_current.dicom_all`
-# WHERE 
-#   SeriesInstanceUID = @series_id;
-# """
-#
-# job_config = bigquery.QueryJobConfig(query_parameters=[
-#                                                        bigquery.ScalarQueryParameter("series_id", "STRING", seriesInstanceUID)
-#                                                        ])
-# result = client.query(query, job_config=job_config) 
-# series_df = result.to_dataframe(create_bqstorage_client=True)
-# print(series_df)
+##################################
+#-- query for crdc_series_uuid --#
+##################################
 
-# client = bigquery.Client(project=project_id)
-#
-# query_view = f"""
-#   SELECT 
-#     * 
-#   FROM
-#     {table_id}
-#   WHERE
-#     SeriesInstanceUID = @series_id;
-#   """
-#
-# job_config = bigquery.QueryJobConfig(query_parameters=[
-#                                                        bigquery.ScalarQueryParameter("series_id", "STRING", seriesInstanceUID)
-#                                                        ])
-# result = client.query(query_view, job_config=job_config) 
-# series_df = result.to_dataframe(create_bqstorage_client=True)
-# print(series_df)
-#
-# gcs_url = series_df['gcs_url'].values
-# gcs_url.to_csv(gcs_file, header = False, index = False)
-#
-# # copy into temporary downloadDirectory
-# import subprocess
-# cmdCommand = "type " + str(gcs_url) + " | gsutil -m cp -I" 
-# process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
-# output, error = process.communicate()
-# print(output)
-# print(error)
+client = bigquery.Client(project=project_id)
 
- 
-#
-# !cat SM_MAX_Series_gcs_manifest.txt | gsutil -m cp -I .
+query = f"""
+SELECT 
+  DISTINCT(crdc_series_uuid) as series_uuid 
+FROM 
+    `bigquery-public-data.idc_current.dicom_all`
+WHERE 
+  SeriesInstanceUID = @series_id;
+"""
 
-# #-- Load the DICOM files into the scene --# 
-#
-# loadedNodeIDs = []  # this list will contain the list of all loaded node IDs
-# from DICOMLib import DICOMUtils
-# with DICOMUtils.TemporaryDICOMDatabase() as db:
-#   DICOMUtils.importDicom(dicomDataDir, db)
-#   patientUIDs = db.patients()
-#   for patientUID in patientUIDs:
-#     loadedNodeIDs.extend(DICOMUtils.loadPatientByUID(patientUID))
-#
-# #-- Load the label files into the scene --# 
-#
-# [slicer.util.loadSegmentation(os.path.join(niftiDataDir,f)) for f in os.listdir(niftiDataDir)]
-#
-# #-- Get the reference volume node --#
-# # The loadedNodeIDs will most likely contain a single scalar volume node, but if not, search for it 
-#
-# referenceVolumeNode = [] 
-# for nodeID in loadedNodeIDs: 
-#   node = slicer.mrmlScene.GetNodeByID(nodeID)
-#   if node.IsA('vtkMRMLScalarVolumeNode'):
-#     referenceVolumeNode = node 
-# if not referenceVolumeNode: 
-#   print('ERROR creating scalar volume from DICOM data directory, please check your data')
-#
-# #-- Create the subject hierarchy --# 
-#
-# # First get the list of segmentationNodes 
-# segmentationNodes = slicer.util.getNodes('*SegmentationNode*') # segmentationNodes = slicer.util.getNodesByClass('*Segmentation*')
-# segmentationNodes = list(segmentationNodes.values())
-#
-# # Get the subject hierarchy 
-# shNode = slicer.mrmlScene.GetSubjectHierarchyNode()
-# seriesItem = shNode.GetItemByDataNode(referenceVolumeNode)
-# studyItem = shNode.GetItemParent(seriesItem)
-# patientItem = shNode.GetItemParent(studyItem)
-#
-# # Set the parent for each of the segmentationNodes 
-# for segmentationNode in segmentationNodes: 
-#   segmentationShItem = shNode.GetItemByDataNode(segmentationNode)
-#   shNode.SetItemParent(segmentationShItem, studyItem)
-#
-# #-- Create the RTStruct --# 
-#
-# if not os.path.isdir(outputDir):
-#   os.mkdir(outputDir)
-#
-# # Get exportables for the segmentations 
-# exporter = DicomRtImportExportPlugin.DicomRtImportExportPluginClass()
-# # exportables = exporter.examineForExport(segmentationShItem) 
-# exportables = [] 
-# [exportables.append(exporter.examineForExport(shNode.GetItemByDataNode(exp))[0]) for exp in segmentationNodes]
-#
-# # Get exportables for the reference volume 
-# referenceVolumeShItem = shNode.GetItemByDataNode(referenceVolumeNode)
-# exportables2 = exporter.examineForExport(referenceVolumeShItem)
-# # Append to a single exportables list 
-# [exportables.append(exp) for exp in exportables2]
-#
-# # Set the outputDir and export 
-# for exp in exportables:
-#   print(exp)
-#   exp.directory = outputDir
-# exporter.export(exportables)
-#
-# print('saved RTStruct')
-#
-# # -- Clean up the scene, necessary? -- # 
+job_config = bigquery.QueryJobConfig(query_parameters=[
+                                                       bigquery.ScalarQueryParameter("series_id", "STRING", referenced_series_instance_uid)
+                                                       ])
+result = client.query(query, job_config=job_config) 
+series_df = result.to_dataframe(create_bqstorage_client=True)
+print(series_df)
 
+########################################## 
+# -- use gsutil to download the files -- #
+##########################################
 
+# first set the downLoad directory 
+downloadDirectory = os.path.join(slicer.dicomDatabase.databaseDirectory, 'tmp')
+if not os.path.isdir(downloadDirectory):
+  os.makedirs(downloadDirectory)
+
+# Use gsutil to copy the files to the downloadDirectory 
+gcs_directory = "gs://public-datasets-idc/" + series_df['series_uuid'].values[0]
+print('gcs_directory: ' + gcs_directory)
+
+# Run gsutil 
+import re
+downloadDirectory = re.sub("/", r"\\", downloadDirectory)
+args = [shutil.which("gsutil")]
+subcommand = f" -m cp -r {gcs_directory}/* {downloadDirectory}"
+args.extend(subcommand.split())
+print(args)
+print(args)
+process = slicer.util.launchConsoleProcess(args)
+print(process)
+process.stdout.read()
+
+#################################################
+# Now add the CT files to the DICOM database -- # 
+#################################################
+    
+DICOMUtils.importDicom(downloadDirectory,db)
+
+##############################################
+#-- Load the CT DICOM files into the scene --# 
+##############################################
+
+loadedNodeIDs = []  # this list will contain the list of all loaded node IDs
+patientUIDs = db.patients()
+for patientUID in patientUIDs:
+    loadedNodeIDs.extend(DICOMUtils.loadPatientByUID(patientUID))
 
